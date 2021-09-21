499082cfdd94deea030241381e71d4a7637065ff
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-09-20 22:23:57 -0400

    Adds test functionality for hiding all cards except the first one

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const searchButton = document.getElementById('search-button');
+const jobsContainer = document.getElementById('movies-container');
 
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -14,9 +15,51 @@ searchButton.addEventListener('click', (e) => {
       console.log(selectJobEncoded);
       console.log(locationEncoded);
       console.log(data);
-      // renderData fn will go here
-      // should onswipe fn go here? we only want 1 card loaded until swiped. 
-      // another event listener needed? 
+      const jobs = renderJobs(data.results);
+      jobsContainer.innerHTML = jobs;
+
+      // **Test** Makes it so only one job card shows
+      const singleJob = document.getElementsByClassName('movieJob');
+      console.log(singleJob);
+      function showOneJob() {
+      for (let i = 1; i < singleJob.length; i++) {
+        singleJob.item(i).classList.add('d-none');
+      }
+    }
+    showOneJob();
     })
+})
+
+// **Test** This is a placeholder for Max's movie card and function
+function renderJobs(jobArray) {
+  const jobHtmlArray = jobArray.map(function (currentJob) {
+    return `<div class="movieJob col p-3">
+<div class="card bg-secondary text-light" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${currentJob.company.name}</h5>
+  <p class="card-text">${currentJob.name}</p>
+  <a href="#" class="add-button btn btn-warning text job-card">Test</a>
+</div>
+</div>
+</div>`
+  })
+  return jobHtmlArray.join('');
+}
+
+// **Test** Need to keep working on this
+jobsContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+  const jobCard = document.getElementsByClassName('job-card');
+  nextJob(singleJob, 0);
 
 })
+function nextJob(arr, index) {
+  // index n disappears
+  // index m appears
+  arr.item(index).classList.add('d-none');
+  arr.item(index + 1).classList.remove('d-none');
+}
+
+
+
+
