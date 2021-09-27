8c22e24ce6bf1ca792aea69feb303432357cd432
Lakeysha <82991137+lakeyshaa@users.noreply.github.com>
2021-09-27 10:42:58 -0400

    added modal

--- favorites.js
+++ favorites.js
@@ -9,21 +9,21 @@ if (!favlist) {
 // if there is not anything in the fav list make sure it is an array so favHTML does not fail
 /* 
 */
-document.querySelector('.jobs-container').innerHTML = renderJobs(favlist);
+document.querySelector('#jobs-list').innerHTML = renderJobs(favlist);
 /* jobs-container is a div where our selected jobs will show up,
 renderJobs passing in fav-list returns a string, innerHTML needs a string to render HTML,
 
 */
 
 function renderJobs(jobs){
-    const html = jobs.map(function(job) {
+    const html = jobs.map(function(job, jobId) {
         // job is our object, it is iterating over an array and it is rendering the data from the keys that belong on the object
          return `
-            <div class='d-flex flex-column mx-auto'> 
-                <h2>${job.company}</h2>
+            <li class='job-item'> 
+                <h2 id="${jobId}">${job.company}</h2>
                 <p>Job Title: ${job.position}</p>
                 <p>Job Level: ${job.level}</p>
-            <div>
+            </li>
         `
      });
     
@@ -32,15 +32,6 @@ function renderJobs(jobs){
       that is why we convert the array of objects to a string*/
  }
 
-
-// function removeMovie(imdbID) {
-//     console.log(watchlist, imdbID)
-//     let results = watchlist.filter(movie => movie.imdbID !== imdbID)
-//     watchlist = results
-//     document.querySelector('.movies-container').innerHTML = renderMovies(results);
-//     watchlistJSON = JSON.stringify(results);
-//     localStorage.setItem('watchlist', watchlistJSON);
-// }
 
 const clearFavlist = document.getElementById('clear-favlist');
 clearFavlist.addEventListener('click', (e) => {
@@ -51,3 +42,49 @@ clearFavlist.addEventListener('click', (e) => {
   <p>Add some jobs!</p>
   </div>`
 })
+
+/*Modal placed here by Keysha*/
+
+const jobs = document.getElementById('jobs-list');
+jobs.addEventListener('click', openModal);
+
+function openModal(e) {
+    if (e.target.tagName === 'H2') {
+        const target = e.target.getAttribute('id');
+        renderDetail(favlist[target].description);
+    }
+}
+
+function renderDetail(description) {
+    let modal = document.getElementById('modal');
+
+    if (modal) {
+        modal.remove();
+    }
+
+modal = document.createElement('div');
+modal.id = "modal";
+modal.className = 'modal';
+modal.innerHTML = `<span class='cancel' id='cancel')&times;</span>
+<p>${description}</p>`;
+
+jobs.append(modal);
+renderCancelBtn(modal);
+}
+
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.removeEventListener('click', closeModal);
+    modal.remove();
+}
+
+    function renderCancelBtn(parentEl) {
+        const button = document.createElement('span');
+        button.addEventListener('click', closeModal);
+        button.className = 'cancel';
+        button.innerHTML = '&times;';
+        parentEl.append(button);
+      }
+      
+    
